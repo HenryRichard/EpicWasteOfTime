47f153053ff0e1ee5e975b03718f359449ddb66f
HenryRichard <ahenryrichard@gmail.com>
2020-04-11 00:12:34 -0500

    Fake anvil now references vanilla anvil for shape

--- src/main/java/henryrichard/epicwasteoftime/block/FakeAnvilBlock.java
+++ src/main/java/henryrichard/epicwasteoftime/block/FakeAnvilBlock.java
@@ -23,15 +23,6 @@ import java.util.Random;
 public class FakeAnvilBlock extends FallingBlock {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
     public static final BooleanProperty DECAY = BooleanProperty.create("decay");
-    private static final VoxelShape PART_BASE = Block.makeCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 4.0D, 14.0D);
-    private static final VoxelShape PART_LOWER_X = Block.makeCuboidShape(3.0D, 4.0D, 4.0D, 13.0D, 5.0D, 12.0D);
-    private static final VoxelShape PART_MID_X = Block.makeCuboidShape(4.0D, 5.0D, 6.0D, 12.0D, 10.0D, 10.0D);
-    private static final VoxelShape PART_UPPER_X = Block.makeCuboidShape(0.0D, 10.0D, 3.0D, 16.0D, 16.0D, 13.0D);
-    private static final VoxelShape PART_LOWER_Z = Block.makeCuboidShape(4.0D, 4.0D, 3.0D, 12.0D, 5.0D, 13.0D);
-    private static final VoxelShape PART_MID_Z = Block.makeCuboidShape(6.0D, 5.0D, 4.0D, 10.0D, 10.0D, 12.0D);
-    private static final VoxelShape PART_UPPER_Z = Block.makeCuboidShape(3.0D, 10.0D, 0.0D, 13.0D, 16.0D, 16.0D);
-    private static final VoxelShape X_AXIS_AABB = VoxelShapes.or(PART_BASE, PART_LOWER_X, PART_MID_X, PART_UPPER_X);
-    private static final VoxelShape Z_AXIS_AABB = VoxelShapes.or(PART_BASE, PART_LOWER_Z, PART_MID_Z, PART_UPPER_Z);
 
     public FakeAnvilBlock(Block.Properties properties) {
         super(properties);
@@ -62,8 +53,7 @@ public class FakeAnvilBlock extends FallingBlock {
 
     @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
-        Direction direction = state.get(FACING);
-        return direction.getAxis() == Direction.Axis.X ? X_AXIS_AABB : Z_AXIS_AABB;
+        return Blocks.ANVIL.getShape(Blocks.ANVIL.getDefaultState().with(FACING, state.get(FACING)), worldIn, pos, context);
     }
 
     @Override
